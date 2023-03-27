bbd334fa2234d5751dcfef00f1b348e8dd9343a2
Rawda Yasser <rawda.developer@gmail.com>
2023-03-27 10:01:08 +0200

    Feat: add controllers for video streaming

--- backend/src/controllers/media.controller.js
+++ backend/src/controllers/media.controller.js
@@ -38,3 +38,76 @@ export const create = (req, res) => {
     }
   });
 };
+export const list = async (req, res) => {
+  try {
+    let media = await Media.find()
+      .populate("postedBy", "_id name")
+      .select("title description genre views created updated")
+      .sort({ created: -1 });
+    res.json(media);
+  } catch (err) {
+    return res.status(400).json({
+      error: "Something went wrong",
+    });
+  }
+};
+export const mediaByID = async (req, res, next, id) => {
+  const media = await Media.findById(id).populate("postedBy", "_id name");
+  if (!media)
+    return res.status("401").json({
+      error: "Media not found",
+    });
+  req.media = media;
+  let files = await gridfs.find({ filename: media._id }).toArray();
+  if (!files[0]) {
+    return res.status(404).send({
+      error: "No video found",
+    });
+  }
+  req.file = files[0];
+  next();
+};
+
+export const video = (req, res) => {
+  const range = req.headers["range"];
+  if (range && typeof range === "string") {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const partialstart = parts[0];
+    const partialend = parts[1];
+
+    const start = parseInt(partialstart, 10);
+    const end = partialend ? parseInt(partialend, 10) : req.file.length - 1;
+    const chunksize = end - start + 1;
+
+    res.writeHead(206, {
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Range": "bytes " + start + "-" + end + "/" + req.file.length,
+      "Content-Type": req.file.contentType,
+    });
+
+    let downloadStream = gridfs.openDownloadStream(req.file._id, {
+      start,
+      end: end + 1,
+    });
+    downloadStream.pipe(res);
+    downloadStream.on("error", () => {
+      res.sendStatus(404);
+    });
+    downloadStream.on("end", () => {
+      res.end();
+    });
+  } else {
+    res.header("Content-Length", req.file.length);
+    res.header("Content-Type", req.file.contentType);
+
+    let downloadStream = gridfs.openDownloadStream(req.file._id);
+    downloadStream.pipe(res);
+    downloadStream.on("error", () => {
+      res.sendStatus(404);
+    });
+    downloadStream.on("end", () => {
+      res.end();
+    });
+  }
+};
--- backend/src/routes/media.route.js
+++ backend/src/routes/media.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { create } from "../controllers/media.controller";
+import { create, list, mediaByID, video } from "../controllers/media.controller";
 import { hasAuthorization, requireLogin } from "../controllers/auth.controller";
 import { userByID } from "../controllers/user.controller";
 const mediaRouter = Router();
 
-mediaRouter.route("/users/:userId/media/").post(requireLogin, hasAuthorization, create);
+mediaRouter
+  .route("/users/:userId/media/")
+  .post(requireLogin, hasAuthorization, create)
+  .get(requireLogin, hasAuthorization, list);
+mediaRouter.route("/media/:mediaId").get(video);
 mediaRouter.param("userId", userByID);
+mediaRouter.param("mediaId", mediaByID);
 export default mediaRouter;
