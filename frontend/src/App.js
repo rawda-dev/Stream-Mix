84c9fa56c45915e23c3aa74f45ca43b7848f660a
Rawda Yasser <rawda.developer@gmail.com>
2023-03-28 01:13:48 +0200

    Feat: add Register card component design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+// import vectorBackground from "./assets/vector_background.png";
+import { RegisterCard } from "./components/RegisterCard";
 function App() {
-  return <p className="font-gray-800 text-center text-xl">Stream Mix</p>;
+  return (
+    <div className="font-gray-800 text-center lg:bg-no-repeat h-screen flex flex-col items-center justify-center lg:bg-[url('./assets/vector_background.png')]">
+      <RegisterCard />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/RegisterCard.js
@@ -0,0 +1,26 @@
+import logo from "../assets/logo.png";
+export const RegisterCard = () => {
+  return (
+    <div className="flex flex-col space-y-10 items-center justify-center lg:w-1/3  md:w-2/3 sm:w-full lg:h-auto mx-auto p-10 bg-white shadow-2xl">
+      <img src={logo} alt="Stream Mix" className="w-32" />
+      <input
+        type="text"
+        className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-transparent "
+        placeholder="Name"
+      />
+      <input
+        type="email"
+        className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-transparent "
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-transparent "
+        placeholder="Password"
+      />
+      <button className="flex-shrink-0 w-full px-4 py-2 text-base font-semibold text-white bg-rose-400 rounded-lg shadow-md hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 focus:ring-offset-purple-200">
+        Register
+      </button>
+    </div>
+  );
+};
