a5fc062f9dc323848d17a138989b4f65c8a90b61
Rawda Yasser <rawda.developer@gmail.com>
2023-03-28 01:22:56 +0200

    Feat: add LoginCard component design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import vectorBackground from "./assets/vector_background.png";
 import { RegisterCard } from "./components/RegisterCard";
+import { LoginCard } from "./components/LoginCard";
 function App() {
   return (
     <div className="font-gray-800 text-center lg:bg-no-repeat h-screen flex flex-col items-center justify-center lg:bg-[url('./assets/vector_background.png')]">
-      <RegisterCard />
+      <LoginCard />
     </div>
   );
 }
--- frontend/src/components/LoginCard.js
+++ frontend/src/components/LoginCard.js
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-export const RegisterCard = () => {
+export const LoginCard = () => {
   return (
     <div className="flex flex-col space-y-10 items-center justify-center lg:w-1/3  md:w-2/3 sm:w-full lg:h-auto mx-auto p-10 bg-white shadow-2xl">
       <img src={logo} alt="Stream Mix" className="w-32" />
@@ -8,11 +8,7 @@ export const RegisterCard = () => {
         className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-transparent "
         placeholder="Name"
       />
-      <input
-        type="email"
-        className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-transparent "
-        placeholder="Email"
-      />
+
       <input
         type="password"
         className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-transparent "
@@ -22,8 +18,8 @@ export const RegisterCard = () => {
         Register
       </button>
       <p>
-        Already have an account?{" "}
-        <span className="text-rose-400 hover:cursor-pointer hover:underline">Login</span>
+        Don't have an account?{" "}
+        <span className="text-rose-400 hover:cursor-pointer hover:underline">Register</span>
       </p>
     </div>
   );
